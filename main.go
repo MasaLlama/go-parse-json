ae6e43e94682097f84b8080daf44cee45bfa2918
Sam Mamelli <cerafina@officialcerafina.com>
2022-01-12 10:22:45 -0500

    initial commit

--- main.go
+++ main.go
@@ -30,12 +30,12 @@ type Listing struct {
 
 func main() {
 	// Open our jsonFile
-	jsonFile, err := os.Open("test.json")
+	jsonFile, err := os.Open("example.json")
 	// if we os.Open returns an error then handle it
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println("Successfully Opened users.json")
+	fmt.Println("Successfully Opened example.json")
 	// defer the closing of our jsonFile so that we can parse it later on
 	defer jsonFile.Close()
 
@@ -46,9 +46,12 @@ func main() {
 
 	fmt.Println(result["Listings"])
 	fmt.Println(result["num_listings"])
-	//birdJson := `[{ "num_listings": "36 ","City": "Toronto","Neighbourhood": "Downtown","Area": "Moss Park"},{"num_listings":"39","City":"Toronto","Neighbourhood":"Downtown","Area":"The Waterfront"}]`
-	//var listing []Listing
-	//json.Unmarshal([]byte(birdJson), &listing)
-	//fmt.Printf("Listing %+v", listing)
+	//`[{ "num_listings": "36 ","City": "Toronto","Neighbourhood": "Downtown","Area": "Moss Park"},{"num_listings":"39","City":"Toronto","Neighbourhood":"Downtown","Area":"The Waterfront"}]`
+	//
+	//
+	//Example output
+	//[map[Address:1007 - 219 Dundas  St E Bathrooms:2BA Bedrooms:3BD Brokerage:LIVING REALTY INC., BROKERAGE MLS_num:C5467882 Parking:0 Parking Price:3,200 Size:800-899 sqft timeListed:11 hours timeScraped:01/10/2022-00:13:38]
+	//map[Address:2208 - 219 Dundas St E Bathrooms:1BA Bedrooms:1+1BD Brokerage:LIVING REALTY INC., BROKERAGE MLS_num:C5467667 Parking:0 Parking Price:2,400 Size:500-599 sqft timeListed:1 day timeScraped:01/10/2022-00:13:38]]
+	//
 
 }
